e0f43e89f7057d4bfcdffc4525ef5501d3b498cb
t0hka <s4ndalph0n.t0hka@gmail.com>
2022-07-28 15:38:53 +0800

    Exec input code

--- Actuator/actuate.go
+++ Actuator/actuate.go
@@ -1,1 +1,14 @@
 package Actuator
+
+import (
+	"8086AssmblySimulator/Classifier"
+	"8086AssmblySimulator/Definer"
+	"8086AssmblySimulator/Parser"
+)
+
+func Actuate(code string) {
+	Parser.Parse(code)
+	println(code)
+	Classifier.Classify(&Parser.OP)
+	Definer.CPU.ShowReg()
+}
