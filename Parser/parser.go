3681f0a7ebc22be1a64acffd38775f3c9ffc73ab
t0hka <s4ndalph0n.t0hka@gmail.com>
2022-07-28 14:52:35 +0800

    Parser input file(.asm)

--- Parser/parser.go
+++ Parser/parser.go
@@ -1,1 +1,26 @@
 package Parser
+
+import (
+	"strings"
+)
+
+var (
+	OP Op
+)
+
+type Op struct {
+	Opcode string
+	Src    string
+	Dst    string
+}
+
+func Parse(code string) {
+	opcode := strings.Split(code, ` `)[0]
+	OP.Opcode = opcode
+	//fmt.Printf("opcode:%s\n", opcode)
+	dst := strings.Split(strings.Split(code, ` `)[1], `,`)[0]
+	src := strings.Split(strings.Split(code, ` `)[1], `,`)[1]
+	//fmt.Printf("src:%s\ndst:%s\n", src, dst)
+	OP.Src = src
+	OP.Dst = dst
+}
