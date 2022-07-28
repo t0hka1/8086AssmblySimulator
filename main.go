670f9ec4355f8b31d6a2ac45265a361e31a60e88
t0hka <s4ndalph0n.t0hka@gmail.com>
2022-07-28 14:53:04 +0800

    Revise function's call

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"8086AssmblySimulator/Definer"
+	"8086AssmblySimulator/Parser"
 )
 
 var CPU Definer.Register
@@ -12,4 +13,13 @@ func main() {
 	//src.MM[0] = 0x1
 	//src.MM[1] = 0x2
 	//src.ReadMemoryWord(0)
+	//  可以将mov ax,18 解析成 下面16-19 行可以封装起来 放到解析器去做
+	//Definer.Src.Reg = &CPU
+	//Definer.Src.Reg1 = &CPU.Ax
+	//Definer.Src.Reg2 = &CPU.Si
+	//Definer.Dst.IMM = 0xff11
+	//Definer.Mov(&Definer.Src, &Definer.Dst, 0)
+	//CPU.ShowReg()
+	code := "mov ax,18"
+	Parser.Parse(code)
 }
