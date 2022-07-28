0e3c79fed04296267f8b42ee46ce1e9bc56cb76e
t0hka <s4ndalph0n.t0hka@gmail.com>
2022-07-28 15:40:56 +0800

    A simple mov example

--- Classifier/classify.go
+++ Classifier/classify.go
@@ -1,1 +1,19 @@
 package Classifier
+
+import (
+	"8086AssmblySimulator/Definer"
+	"8086AssmblySimulator/Parser"
+	"strconv"
+)
+
+func Classify(op *Parser.Op) {
+	if op.Opcode == "mov" { // mov 操作码
+		Definer.Src.Reg = &Definer.CPU
+		if op.Dst == "ax" {
+			Definer.Dst.Reg1 = &Definer.CPU.Ax
+			tmp, _ := strconv.Atoi(op.Src)
+			Definer.Src.IMM = uint16(tmp)
+			Definer.Mov(&Definer.Src, &Definer.Dst, 0)
+		}
+	}
+}
